949e7cac7e7be9169f67e237702b316ee335f53b
IraTrachuk <iratrachuk@online.ua>
2015-04-06 20:51:34 +0300

    Lection 6

---
+++ src/com/company/Easy.java
@@ -0,0 +1,18 @@
+package com.company;
+
+/**
+ * Created by Admin on 06.04.15.
+ */
+public class Easy {
+    public static void main(String[] args) {
+        System.out.println(sum(5, 7));
+        System.out.println(sum(35, 23));
+    }
+
+    public static int sum(int a, int b){
+        int res = a + b;
+        return res;
+    }
+}
+
+
